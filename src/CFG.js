c6b1c62d0f0123829f94f784335e573d922f2c61
matheuscorreia <matheuscorreia2005@gmail.com>
2018-05-12 12:20:46 -0300

    fix(class): remove vanity properties

--- src/CFG.js
+++ src/CFG.js
@@ -2,57 +2,20 @@
 import type {
   ContextFreeGrammar,
   NonTerminalSymbol,
-  TerminalSymbol,
   ProductionRule,
   StartSymbol,
   Symbol,
 } from './types'
 
 export default class CFG {
-  terminalSymbols: TerminalSymbol[]
-  nonTerminalSymbols: NonTerminalSymbol[]
-  productionRules: [NonTerminalSymbol, Symbol[]][]
-  startSymbols: NonTerminalSymbol[]
-  addNonTerminalSymbol: (NonTerminalSymbol, Symbol[]) => void
-  addTerminalSymbol: (NonTerminalSymbol, Symbol[]) => void
-  addStartSymbol: (NonTerminalSymbol, Symbol[]) => void
+  productionRules: ProductionRule[]
   addProductionRule: (NonTerminalSymbol, Symbol[]) => void
   generate: () => string
 
   constructor({
-    terminalSymbols = [],
-    nonTerminalSymbols = [],
     productionRules = [],
-    startSymbols = [],
   }: ContextFreeGrammar) {
-    this.terminalSymbols = terminalSymbols
-    this.nonTerminalSymbols = nonTerminalSymbols
     this.productionRules = productionRules
-    this.startSymbols = startSymbols
-  }
-
-  addTerminalSymbol(x: TerminalSymbol | TerminalSymbol[]) {
-    if (Array.isArray(x)) {
-      this.terminalSymbols.push(...x);
-    } else {
-      this.terminalSymbols.push(x)
-    }
-  }
-
-  addNonTerminalSymbol(x: NonTerminalSymbol | NonTerminalSymbol[]) {
-    if (Array.isArray(x)) {
-      this.nonTerminalSymbols.push(...x);
-    } else {
-      this.nonTerminalSymbols.push(x)
-    }
-  }
-
-  addStartSymbol(x: StartSymbol | StartSymbol[]) {
-    if (Array.isArray(x)) {
-      this.nonTerminalSymbols.push(...x);
-    } else {
-      this.nonTerminalSymbols.push(x)
-    }
   }
 
   addProductionRule(x: NonTerminalSymbol, y: Symbol[]) {
