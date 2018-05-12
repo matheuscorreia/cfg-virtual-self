ae02568dabad2653717921c941db29a8e397926f
matheuscorreia <matheuscorreia2005@gmail.com>
2018-05-12 12:32:34 -0300

    feat(grammar): create grammar file

---
+++ src/grammar.js
@@ -0,0 +1,64 @@
+// @flow
+
+const nonTerminalSymbols = {
+  S: 'S',
+  VP: 'VP',
+  NP: 'NP',
+  PP: 'PP',
+  DT: 'DT',
+  Vi: 'Vi',
+  Vt: 'Vt',
+  NN: 'NN',
+  IN: 'IN',
+}
+
+const terminalSymbols = {
+  sleeps: 'sleeps',
+  saw: 'saw',
+  man: 'man',
+  woman: 'woman',
+  telescope: 'telescope',
+  the: 'the',
+  with: 'with',
+  in: 'in',
+}
+
+const {
+  S,
+  VP,
+  NP,
+  PP,
+  DT,
+  Vi,
+  Vt,
+  NN,
+  IN,
+} = nonTerminalSymbols
+
+// small alias for making referencing easier
+const ts = terminalSymbols
+
+export const productionRules = [
+  [S, [NP, VP]],
+  //---
+  [VP, [Vi]],
+  [VP, [Vt, NP]],
+  [VP, [VP, PP]],
+  //---
+  [NP, [DT, NN]],
+  [NP, [NP, PP]],
+  //---
+  [PP, [IN, NP]],
+  //---
+  [Vi, [ts.sleeps]],
+  [Vt, [ts.saw]],
+  //---
+  [NN, [ts.man]],
+  [NN, [ts.woman]],
+  [NN, [ts.telescope]],
+  //---
+  [DT, [ts.the]],
+  //---
+  [IN, [ts.with]],
+  [IN, [ts.in]],
+]
